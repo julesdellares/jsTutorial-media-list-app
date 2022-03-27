9783ef2d03cc25f5ebff281cb88ce878e5f56338
jules <101470183+julesdellares@users.noreply.github.com>
2022-03-26 21:16:44 -0700

    Update app.js
    added event to add book. a little rushed today, but i'm sure i'll learn mor ein debugging tommorow

--- app.js
+++ app.js
@@ -44,7 +44,19 @@ class UI {
 //Store Class: Handles Storage
 
 //Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //Event: Add a Book
+
+document.querySelector('#book-form').addEventListener('submit',(e) => {
+    //get form values
+    const title = document.querySelector(#title).value;
+    const author = document.querySelector(#author).value;
+    const isbn = document.querySelector(#isbn).value;
+
+    //instantiate book
+
+    const book = new Book(title, author, isbn);
+});
 
 //Event: Remove A Book
